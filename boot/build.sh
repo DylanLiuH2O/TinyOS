e87a83dc28e6c18c7cd88e4b875bfffe14b6d16f
Dylan-Linux <913464928@qq.com>
2021-03-19 09:01:45 +0800

    add build.sh

---
+++ boot/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+INCLUDE=include/
+USRHOME=/home/dylan
+
+echo "building mbr..."
+nasm -I ${INCLUDE} -o mbr.bin mbr.S
+
+echo "building loader..."
+nasm -I ${INCLUDE} -o loader.bin loader.S
+
+echo "building img..."
+dd if=./mbr.bin of=${USRHOME}/bochs/bin/hd60M.img bs=512 count=1 conv=notrunc
+dd if=./mbr.bin of=${USRHOME}/bochs/bin/hd60M.img bs=512 count=1 seek=2 conv=notrunc
+
+echo "build successfully!"
