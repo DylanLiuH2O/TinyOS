d9daabf50e941eee01adb0b3b3bcbdaefdfacc32
Dylan-Linux <913464928@qq.com>
2021-03-19 10:24:06 +0800

    change count=1 -> count=4 when compiling loader.S

--- boot/build.sh
+++ boot/build.sh
@@ -10,6 +10,6 @@ nasm -I ${INCLUDE} -o loader.bin loader.S
 
 echo "building img..."
 dd if=./mbr.bin of=${USRHOME}/bochs/bin/hd60M.img bs=512 count=1 conv=notrunc
-dd if=./loader.bin of=${USRHOME}/bochs/bin/hd60M.img bs=512 count=1 seek=2 conv=notrunc
+dd if=./loader.bin of=${USRHOME}/bochs/bin/hd60M.img bs=512 count=4 seek=2 conv=notrunc
 
 echo "build successfully!"
