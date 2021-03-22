3205f2772b259f4fb8a92ed425b8ecca3c30c507
Dylan-Linux <913464928@qq.com>
2021-03-22 21:09:56 +0800

    update build.sh

--- boot/build.sh
+++ boot/build.sh
@@ -10,6 +10,6 @@ nasm -I ${INCLUDE} -o loader.bin loader.S
 
 echo "building img..."
 dd if=./mbr.bin of=${USRHOME}/bochs/bin/hd60M.img bs=512 count=1 conv=notrunc
-dd if=./loader.bin of=${USRHOME}/bochs/bin/hd60M.img bs=512 count=300 seek=2 conv=notrunc
+dd if=./loader.bin of=${USRHOME}/bochs/bin/hd60M.img bs=512 count=200 seek=2 conv=notrunc
 
 echo "build successfully!"
