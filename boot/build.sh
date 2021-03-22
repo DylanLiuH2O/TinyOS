a1d919597f282f9c47bc18ec828c1efd1bf45629
dylan <913464928@qq.com>
2021-03-23 01:35:08 +0800

    update build.sh

--- boot/build.sh
+++ boot/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 INCLUDE=include/
 USRHOME=/home/dylan
+OS=/home/dylan/Workspaces/repo/OS/hd
 
 echo "building mbr..."
 nasm -I ${INCLUDE} -o mbr.bin mbr.S
@@ -9,7 +10,7 @@ echo "building loader..."
 nasm -I ${INCLUDE} -o loader.bin loader.S
 
 echo "building img..."
-dd if=./mbr.bin of=${USRHOME}/bochs/bin/hd60M.img bs=512 count=1 conv=notrunc
-dd if=./loader.bin of=${USRHOME}/bochs/bin/hd60M.img bs=512 count=200 seek=2 conv=notrunc
+dd if=./mbr.bin of=${OS}/hd60M.img bs=512 count=1 conv=notrunc
+dd if=./loader.bin of=${OS}/hd60M.img bs=512 count=200 seek=2 conv=notrunc
 
 echo "build successfully!"
