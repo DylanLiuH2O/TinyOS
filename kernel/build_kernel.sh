9b86dfe13a2008af678a1f488457d6994c06d756
dylan <913464928@qq.com>
2021-03-24 18:26:27 +0800

    modify build_kernel

--- kernel/build_kernel.sh
+++ kernel/build_kernel.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+LIB=/home/dylan/Workspaces/repo/OS/lib
 TOOL=/home/dylan/Workspaces/repo/OS/tool
 KEL=/home/dylan/Workspaces/repo/OS/kernel
+HD=/home/dylan/Workspaces/repo/OS/hd
 
-${TOOL}/gcc32.sh main.c main.o
-${TOOL}/ld32.sh main.o 0xc0001500 main kernel.bin
+nasm -f elf -o ${LIB}/kernel/print.o ${LIB}/kernel/print.S
+gcc -m32 -fno-builtin -nostdlib -nostdinc -fno-stack-protector -c main.c -o main.o
+ld -m elf_i386 ${KEL}/main.o ${LIB}/kernel/print.o -Ttext 0xc0001500 -e main -o kernel.bin
 
-dd if=${KEL}/kernel.bin of=/home/dylan/bochs/bin/hd60M.img bs=512 count=200 seek=9 conv=notrunc
+dd if=${KEL}/kernel.bin of=${HD}/hd60M.img bs=512 count=200 seek=9 conv=notrunc
