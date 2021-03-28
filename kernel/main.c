9bf573ccf76ddf6d6851a56d29eacc6d74c0d182
dylan <913464928@qq.com>
2021-03-28 22:59:08 +0800

    delete useless files

--- kernel/main.c
+++ kernel/main.c
@@ -1,22 +1,8 @@
 #include "../lib/kernel/print.h"
+#include "init.h"
 int main(void) {
-    /*
-    put_char('k');
-    put_char('e');
-    put_char('r');
-    put_char('n');
-    put_char('e');
-    put_char('l');
-    put_char('\n');
-    put_char('1');
-    put_char('2');
-    put_char('\b');
-    put_char('3');
-    */
-    put_str("kernel\n12\b3");
-    put_char('\n');
-    put_int(0x00000000);
-    put_char('\n');
-    put_int(0x0000abcd);
+    put_str("I am kernel\n");
+    init_all();
+    asm volatile("sti");    //open interrupt
     while(1);
 }
