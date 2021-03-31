63e1a7c1492cf358ad7d30c2720c700def91f8c1
dylan <913464928@qq.com>
2021-03-31 23:19:47 +0800

    improve readability

--- lib/kernel/print.h
+++ lib/kernel/print.h
@@ -1,6 +1,6 @@
 #ifndef _LIB_KERNEL_PRINT_H
 #define _LIB_KERNEL_PRINT_H
-#include "../stdint.h"
+#include "stdint.h"
 void put_char(uint8_t char_ascii);
 void put_str(char* msg);
 void put_int(uint32_t num);
