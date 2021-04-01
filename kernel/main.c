5d399abb60ff324704d1203bb7177a57ae508fa5
dylan <913464928@qq.com>
2021-04-01 22:05:46 +0800

    test ASSERT()

--- kernel/main.c
+++ kernel/main.c
@@ -1,8 +1,11 @@
 #include "print.h"
 #include "init.h"
+#include "debug.h"
+#define DEBUG
 int main(void) {
     put_str("I am kernel\n");
     init_all();
-    asm volatile("sti");    //open interrupt
+    //asm volatile("sti");    open interrupt
+    ASSERT(1 == 2);
     while(1);
 }
