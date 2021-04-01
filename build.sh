b14e03a973e16276a367cd4fe5da447ab367142e
dylan <913464928@qq.com>
2021-04-01 22:04:47 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -4,8 +4,11 @@ HD=/home/dylan/Workspaces/repo/OS/hd
 
 
 nasm -f elf -o build/print.o lib/kernel/print.S
+
 nasm -f elf -o build/kernel.o kernel/kernel.S
 
+
+gcc -m32 -I lib/kernel -I kernel/ -c -fno-builtin -fno-stack-protector -o build/debug.o kernel/debug.c
 
 gcc -m32 -I lib/kernel -I lib/ -c -fno-builtin -fno-stack-protector -o build/timer.o device/timer.c
 
@@ -18,7 +21,7 @@ gcc -m32 -I lib/kernel/ -I lib/ -I kernel/ -c -fno-builtin -fno-stack-protector 
 gcc -m32 -I lib/kernel/ -I lib/ -I kernel/ -I device/ -c -fno-builtin -fno-stack-protector \
     -o build/init.o kernel/init.c
 
-ld -m elf_i386 build/main.o build/kernel.o build/init.o build/interrupt.o build/print.o build/timer.o \
+ld -m elf_i386 build/main.o build/kernel.o build/init.o build/interrupt.o build/print.o build/timer.o build/debug.o \
     -Ttext 0xc0001500 -e main -o build/kernel.bin
 
 dd if=build/kernel.bin of=${HD}/hd60M.img bs=512 count=200 seek=9 conv=notrunc
