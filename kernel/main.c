d8e3bf2849eef354e861ec2ca12f7e269ef3d751
dylan <913464928@qq.com>
2021-04-06 22:04:22 +0800

    test main.c

--- kernel/main.c
+++ kernel/main.c
@@ -9,33 +9,12 @@
 int main(void) {
     put_str("I am kernel\n");
     init_all();
-    //asm volatile("sti");    open interrupt
-    //ASSERT(1 == 2);
-
-    struct bitmap bm; 
-
-    bm.bits = (uint8_t*)0xc009a000;
-    bitmap_init(&bm, 5);
-    bm.bits[0] = 0xff;
-
-    put_str("test bitmap:\n");
-    uint32_t index = bitmap_search(&bm, 10);
-    put_str("  expect: 8");
-    put_str("  actual: ");
-    put_int(index);
+    
+    void* addr = get_kernel_pages(3);
+    put_str("\nget_kernel_page start vaddr is: ");
+    put_int((uint32_t)addr);
     put_char('\n');
-
-    put_str("test bitmap_set:\n");
-    for (int i = 0; i < 10; i++) {
-        bitmap_set(&bm, index+i, 1);
-    }
-    index = bitmap_search(&bm, 10);
-    put_str("  expect: 12");
-    put_str("  actual: ");
-    put_int(index);
-    put_char('\n');
-
-    mem_init(); 
 
     while(1);
+    return 0;
 }
