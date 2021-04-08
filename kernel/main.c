06cb14aa5668524066947cd6b949bebcee9e6504
dylan <913464928@qq.com>
2021-04-08 14:54:43 +0800

    test task shedule

--- kernel/main.c
+++ kernel/main.c
@@ -5,22 +5,38 @@
 #include "string.h"
 #include "bitmap.h"
 #include "memory.h"
+#include "interrupt.h"
 #include "thread.h"
 #define DEBUG
 
 void k_thread_a(void* arg);
+void k_thread_b(void* arg);
 
 int main(void) {
     put_str("I am kernel\n");
     init_all();
     
     thread_start("k_thread_a", 31, k_thread_a, "argA ");
+    thread_start("k_thread_b", 31, k_thread_b, "argB ");
+
+    intr_enable();
+    while(1) {
+        put_str("Main ");
+    }
 
     while(1);
     return 0;
 }
 
 void k_thread_a(void* arg)
+{
+    char* para = (char*)arg;
+    while (1) {
+        put_str(para);    
+    }
+}
+
+void k_thread_b(void* arg)
 {
     char* para = (char*)arg;
     while (1) {
