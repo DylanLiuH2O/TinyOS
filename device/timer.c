5cc28e10f67e1653e1a15647d422a121f927a91c
dylan <913464928@qq.com>
2021-04-08 14:53:11 +0800

    new feature, add timer handle function

--- device/timer.c
+++ device/timer.c
@@ -2,6 +2,10 @@
 #include "print.h"
 #include "io.h"
 #include "stdint.h"
+#include "thread.h"
+#include "interrupt.h"
+#include "debug.h"
+#include "thread.h"
 
 #define IRQ0_FREQUENCY   100
 #define INPUT_FREQUENCY  1193180
@@ -12,6 +16,8 @@
 #define READ_WRITE_LATCH 3
 #define PIT_CONTROL_PORT 0x43
 
+uint32_t ticks;
+
 static void set_frequency(uint8_t counter_port, uint8_t counter_no, \
                           uint8_t rwl, uint8_t counter_mode, \
                           uint16_t counter_value) {
@@ -21,10 +27,27 @@ static void set_frequency(uint8_t counter_port, uint8_t counter_no, \
     outb(counter_port, (uint8_t)counter_value >> 8);
 }
 
-void timer_init(void) {
+static void intr_timer_handler(void)
+{
+    struct task_struct* curr_thread = running_thread();
+
+    //通过判断标记部分是否被覆盖得知栈是否已满
+    ASSERT(curr_thread->stack_magic == 0x19991005);
+
+    curr_thread->elapsed_ticks++;
+    ticks++;
+
+    if (curr_thread->ticks == 0) {
+        schedule(); //时间片为0,调度下一个线程
+    } else {
+        curr_thread->ticks--; 
+    }
+}
+
+void timer_init(void)
+{
     put_str("[timer]: init start\n");
     set_frequency(COUNTER0_PORT, COUNTER0_NO, READ_WRITE_LATCH, COUNTER_MODE, COUNTER0_VALUE);
+    register_handler(0x20, intr_timer_handler);
     put_str("[timer]: init done\n");
-
-
 }
