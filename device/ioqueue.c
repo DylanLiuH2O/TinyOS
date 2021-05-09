5874d825aa6d72df442d0f3b9cec7cdc965f25d1
dylan <913464928@qq.com>
2021-05-09 19:10:09 +0800

    delete comments

--- device/ioqueue.c
+++ device/ioqueue.c
@@ -65,26 +65,3 @@ int32_t next_pos(int32_t pos)
 {
     return (pos + 1) % BUFSIZE;
 }
-
-/*
-_BOOL ioqueue_full(struct ioqueue* aioqueue)
-{
-    return next_pos(aioqueue->head) == aioqueue->tail;
-}
-
-_BOOL ioqueue_empty(struct ioqueue* aioqueue)
-{
-    return aioqueue->head == aioqueue->tail;
-}
-
-void ioqueue_wait(struct task_struct** waiter)
-{
-    *waiter = running_thread();
-    thread_block(TASK_BLOCKED);
-}
-
-void ioqueue_wake(struct task_struct** waiter)
-{
-    thread_unblock(*waiter);
-}
-*/
--- device/ioqueue.h
+++ device/ioqueue.h
@@ -20,12 +20,5 @@ void ioqueue_putchar(struct ioqueue* aioqueue, char ch);
 char ioqueue_getchar(struct ioqueue* aioqueue);
 
 static int32_t next_pos(int32_t pos);
-/*
-static _BOOL ioqueue_full(struct ioqueue* aioqueue);
-static _BOOL ioqueue_empty(struct ioqueue* aioqueue);
-static void ioqueue_wait(struct task_struct** waiter);
-static void ioqueue_wake(struct task_struct** waiter);
-*/
-
 
 #endif
